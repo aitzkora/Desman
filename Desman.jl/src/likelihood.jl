899b2c56c91ccf7f913ed5750dca4328930cbef8
aitzkora <marc.fuentes@inria.fr>
2025-04-18 00:09:35 +0200

    find the bug in gradient_pdf_frailty,
    - pass lots of functions with generics types (enable to use BigFloat),
    - add tests for Gradients

--- Desman.jl/src/likelihood.jl
+++ Desman.jl/src/likelihood.jl
@@ -5,14 +5,14 @@ using Distributions
 using Integrals
 using SpecialFunctions
 
-function weibull_diff(σ::Matrix{Float64}, λ::Vector{Float64}, w::Float64, xd::Float64, xg::Float64)
-  σ_γ=sum(σ*λ[4:end])
+function weibull_diff(σ::Matrix{T}, λ::Vector{T}, w::T, xd::T, xg::T) where {T<:Real}
+  σ_γ=sum(σ*λ[4:end]) # kontuz σ must be a linear form 1xn
   θ = w*(λ[1]+σ_γ)
   α = λ[2]
   return exp(-(xd/θ)^α)-exp(-(xg/θ)^α)
 end
 
-function grad_weibull(σ::Matrix{Float64}, λ::Vector{Float64}, w::Float64, xd::Float64, xg::Float64)
+function grad_weibull(σ::Matrix{T}, λ::Vector{T}, w::T, xd::T, xg::T) where {T<:Real}
   σ_γ=sum(σ*λ[4:end])
   θ = w*(λ[1]+σ_γ)
   α = λ[2]
@@ -23,71 +23,77 @@ function grad_weibull(σ::Matrix{Float64}, λ::Vector{Float64}, w::Float64, xd::
     g[1] += -w*exp(-(xg/θ)^α)*α*(xg/θ)^α/θ
     g[2] += exp(-(xg/θ)^α)*(xg/θ)^α*log(xg/θ)
   end
-  g[3] = 0.0
+  g[3] = zero(T)
   g[4:end]= g[1] .* σ[:]
   return g
 end
 
-function prod_weibull(bio::Biotope, i::Int64, selVar::Vector{Int64}, w::Float64, λ::Vector{Float64})
+function prod_weibull(bio::Biotope, i::Int64, selVar::Vector{Int64}, w::T, λ::Vector{T}) where {T<:Real}
   idx = collect([size(bio.df,2)-size(bio.Σ,2)+2:size(bio.df,2);])
   γ = λ[4:end]
-  z = zeros(length(bio.idx[i]))
+  z = zeros(T,length(bio.idx[i]))
   for (jj, j) in enumerate(bio.idx[i])
-    z[jj] = weibull_diff(Matrix{Float64}(bio.df[j:j,idx[selVar]]), λ, w, 
-                                         bio.df.durationd[j], bio.df.durationg[j])
+      z[jj] = weibull_diff(Matrix{T}(bio.df[j:j,idx[selVar]]), λ, w, T(bio.df.durationd[j]), T(bio.df.durationg[j]))
   end 
   return prod(z)
 end
 
 """
 pdf_frailty(bio, i, selVar)
-computes the density of the `i`-th latrine against the `selVar` collection of covariables
+computes the density of the `i`-th latrine against the `selVar` collection of covariates
 """
-function pdf_frailty(bio::Biotope, i::Int64, selVar::Vector{Int64})
-  function proxyDistCov(w::Float64, λ::Vector{Float64})
+function pdf_frailty(bio::Biotope, i::Int64, selVar::Vector{Int64}, ::Type{T}) where {T<:Real}
+  function proxyDistCov(w::T, λ::Vector{T})
     return prod_weibull(bio,i, selVar,w,λ) * Γh(λ[3], w)
   end
   return proxyDistCov
 end
 
-function grad_pdf_frailty(bio::Biotope, i::Int64, selVar::Vector{Int64})
-  function proxyGrad(w::Float64, λ::Vector{Float64})
+# default type is Float64
+pdf_frailty(bio::Biotope, i::Int64, selVar::Vector{Int64}) = pdf_frailty(bio, i, selVar, Float64)
+
+
+function grad_pdf_frailty(bio::Biotope, i::Int64, selVar::Vector{Int64}, ::Type{T}) where {T<:Real}
+  function proxyGrad(w::T, λ::Vector{T})
     idx = collect([size(bio.df,2)-size(bio.Σ,2)+2:size(bio.df,2);])
     p_wei = prod_weibull(bio, i, selVar, w, λ)
-    ∂pdf = similar(λ)
+    ∂pdf = zeros(size(λ))
     for (jj, j) in enumerate(bio.idx[i])
-        σ = Matrix{Float64}(bio.df[j:j,idx[selVar]])
-        p_exclu_j = p_wei ./ weibull_diff(σ, λ, w, bio.df.durationd[j], bio.df.durationg[j])
-        ∂pdf += p_exclu_j .* grad_weibull(σ, λ, w, bio.df.durationd[j], bio.df.durationg[j])
+        σ = Matrix{T}(bio.df[j:j,idx[selVar]])
+        p_exclu_j = p_wei ./ weibull_diff(σ, λ, w, T(bio.df.durationd[j]), T(bio.df.durationg[j]))
+        ∂pdf += p_exclu_j .* grad_weibull(σ, λ, w, T(bio.df.durationd[j]), T(bio.df.durationg[j]))
     end 
-    ∂pdf[3] = ∂ᵤΓh(λ[3], w)
+    ∂pdf *= Γh(λ[3], w)
+    ∂pdf[3] = p_wei * ∂ᵤΓh(λ[3], w)
     return ∂pdf
   end
   return proxyGrad
 end
 
+grad_pdf_frailty(bio::Biotope, i::Int64, selVar::Vector{Int64}) =  grad_pdf_frailty(bio, i, selVar, Float64)
+
 """
-Γh is a proxy function to speedup the computation of pdf(Gamma(u,1/u))
+Γh is a optimized version of x->pdf(Gamma(u,1/u),x)
 Γh(3.4,5.2) -> 25.503 ns (0 allocations: 0 bytes)
 pdf(Gamma(3.4,1. / 3.4), 5.2) ->  235.772 ns (2 allocations: 48 bytes)
 """
-function Γh(u::Float64, w::Float64)
-    return w^(u-1)*exp(-w*u)*u^u/gamma(u)
+function Γh(u::T, w::T) where {T<:Real}
+  return w^(u-1)*exp(-w*u)*u^u/gamma(u)
 end 
 
-function ∂ᵤΓh(u::Float64,w::Float64)
-    return Γh(u,w)*(log(w*u)-w-digamma(u)+1)
+function ∂ᵤΓh(u::T, w::T) where {T<:Real}
+  return Γh(u,w)*(log(w*u)-w-digamma(u)+1)
 end
 
 """
 logLikelihood(b::Biotope, selVar::Vector{Int64}, methodInt = GaussLegendre)
-computes the logLikelihood conditionned by a subset of covariables given by `selVar`
+computes the logLikelihood conditioned by a subset of covariates given by `selVar`
 """
 function logLikelihood(b::Biotope, selVar::Vector{Int64}, methodInt = GaussLegendre)
   return λ->-sum(log.([Integrals.solve(IntegralProblem(pdf_frailty(b,i,selVar), (0.0, Inf), λ), methodInt())[1] for i=1:b.N]))
 end
 
-function grad_logLikelihood(b::Biotope, selVar::Vector{Int64}, methodInt=QuadGKJL)
+function grad_logLikelihood(b::Biotope, selVar::Vector{Int64}, methodInt = QuadGKJL)
     return λ->sum([solve(IntegralProblem(grad_pdf_frailty(b, i, selVar), (0.0, Inf), λ), methodInt()).u ./
                    solve(IntegralProblem(pdf_frailty(b, i, selVar), (0.0, Inf), λ), methodInt())[1] for i=1:b.N])
 end
--- Desman.jl/test/runtests.jl
+++ Desman.jl/test/runtests.jl
@@ -5,22 +5,27 @@ using DataFrames
 using Distributions
 using SpecialFunctions 
 
+# internal function to build data in common between tests
+function fixture()
+  df1 = CSV.read(joinpath(datapath,"survcalibfeb2024_1.csv"), DataFrame; delim=';', decimal=',');
+  df2 = CSV.read(joinpath(datapath,"survcalibaug2024_1.csv"), DataFrame; delim=';', decimal=',');
+  df  = vcat(df1, df2, cols=:union)
+
+  df.durationg =(x->(x=="NA") ? Inf : parse(Float64,replace(x, "," => "."))).(df[:,:durationg])
+  finite_idx=findall(df.durationg .< Inf)
+  Σ = CSV.read(joinpath(datapath,"Latrine_cov.csv"), DataFrame; delim=";", decimal=',')
+  df = outerjoin(df, Σ; on=:siteID, matchmissing=:equal, makeunique=true)
+  
+  # initial λ
+  λ₀ = sum((df.durationg[finite_idx]+df.durationd[finite_idx])./2.)/length(finite_idx)
+  λ = [ λ₀, 1, 1]
+  bio = Biotope(df, Σ)
+
+  return bio, λ
+end
 
 @testset "likelihood-λ" begin
-    df1 = CSV.read(joinpath(datapath,"survcalibfeb2024_1.csv"), DataFrame; delim=';', decimal=',');
-    df2 = CSV.read(joinpath(datapath,"survcalibaug2024_1.csv"), DataFrame; delim=';', decimal=',');
-    df  = vcat(df1, df2, cols=:union)
-
-    df.durationg =(x->(x=="NA") ? Inf : parse(Float64,replace(x, "," => "."))).(df[:,:durationg])
-    finite_idx=findall(df.durationg .< Inf)
-    Σ = CSV.read(joinpath(datapath,"Latrine_cov.csv"), DataFrame; delim=";", decimal=',')
-    df = outerjoin(df, Σ; on=:siteID, matchmissing=:equal, makeunique=true)
-    
-    # initial λ
-    λ₀ = sum((df.durationg[finite_idx]+df.durationd[finite_idx])./2.)/length(finite_idx)
-    λ = [ λ₀, 1, 1]
-    bio = Biotope(df, Σ)
-    
+    bio, λ = fixture()
     @test logLikelihood(bio, Int64[])(exp.(λ)) ≈  936.14681223 atol = 1e-5
 end
 
@@ -28,3 +33,28 @@ end
    @test pdf(Gamma(3.33, 1. /3.33), 54.) ≈ Γh(3.33, 54.) atol=1e-8
    @test ∂ᵤΓh(3.2,5.1) ≈ (-0.00011584186052754) atol=1e-15
 end
+
+@testset "Gradients" begin
+   using FiniteDiff
+   # weibull
+   nvar = 3
+   σ = rand(1,nvar)
+
+   w = (rand(1)*10 .+ 10)[1]
+
+   λ = rand(3+nvar)
+   xd = rand(1)[1]
+   xg = rand(1)[1]
+   @test FiniteDiff.finite_difference_gradient(x->weibull_diff(σ,x, w, xg, xd), λ) ≈ grad_weibull(σ, λ, w, xg, xd) atol=1e-10
+
+   # pdf_frailty
+  
+   bio, λ = fixture()
+   i = 12
+   selVar = [3,4];
+   γ = rand(2)
+   g = pdf_frailty(bio, i, selVar)
+   w = w
+   μ = [λ;γ]
+   @test FiniteDiff.finite_difference_gradient(x->g(w,x), μ) ≈ grad_pdf_frailty(bio,i, selVar)(w, μ) atol=1e-10
+end  
